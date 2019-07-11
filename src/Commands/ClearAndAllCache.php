fad2fd3bbe682df23d15635e4ea30258c4ddca6b
Htin Lynn <htinlin01@gmail.com>
2019-07-11 06:54:07 +0000

    Apply fixes from StyleCI

--- src/Commands/ClearAndAllCache.php
+++ src/Commands/ClearAndAllCache.php
@@ -3,16 +3,16 @@
  * Created by PhpStorm.
  * User: htinlynn
  * Date: 11/28/18
- * Time: 4:12 PM
+ * Time: 4:12 PM.
  */
 
 namespace Genius\Commands;
 
 use Arcanedev\Support\Bases\Command;
 use Illuminate\Support\Facades\Artisan;
+
 class ClearAndAllCache extends Command
 {
-
     /**
      * The name and signature of the console command.
      *
@@ -41,6 +41,7 @@ class ClearAndAllCache extends Command
         Artisan::call('config:cache');
         $this->frame('All caches of config, route, view and cache are cleared.');
         $this->frame('And config cache');
+
         return 'Success';
     }
 }
